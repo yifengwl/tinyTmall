2493cd0e1fcd0fb831c8b5111d8043c8ef20ed25
wanglu <1048274411@qq.com>
2017-02-23 17:22:08 +0800

    add backgroundData

--- src/main/java/com/wanglu/tmall/controller/UserController.java
+++ src/main/java/com/wanglu/tmall/controller/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class UserController {
-
     @RequestMapping(value = "register", method = RequestMethod.GET)
     public String registerPage( ModelMap map){
         User user = new User();
